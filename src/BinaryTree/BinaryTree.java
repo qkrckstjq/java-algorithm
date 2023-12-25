90e278fb746d73cfa65ea4058e3cfc0ee8d9e26b
qkrckstjq <cksk49@naver.com>
2023-12-25 17:32:13 +0900

    feat BT에 값 삽입 구현

---
+++ src/BinaryTree/BinaryTree.java
@@ -0,0 +1,58 @@
+package BinaryTree;
+
+import java.util.Arrays;
+
+public class BinaryTree {
+    private int defaultSize = 10;
+    private Integer[] BT;
+    public BinaryTree() {
+        this.BT = new Integer[defaultSize];
+        this.BT[0] = null;
+    }
+    public void insert(int number) {
+        int currentIndex = 1;
+
+        while (currentIndex < BT.length) {
+            if (BT[currentIndex] == null) {
+                BT[currentIndex] = number;
+                break;
+            } else if (number < BT[currentIndex]) {
+                currentIndex = currentIndex * 2; // 왼쪽 자식으로 이동
+            } else if (number > BT[currentIndex]) {
+                currentIndex = currentIndex * 2 + 1; // 오른쪽 자식으로 이동
+            } else {
+                break;
+            }
+        }
+
+        // 배열 크기가 부족하면 확장
+        if (currentIndex >= BT.length) {
+            arrExtends(BT.length * 2);
+        }
+    }
+
+    public void findValueIndex (int number) {
+        int node = 1;
+        while(BT[node] != null) {
+            if(BT[node] == number) {
+                System.out.printf("%d은 %d번째 인덱스에 위치함\n", number, node);
+                return;
+            }
+            if(number < BT[node]) {
+                node = node * 2;
+            } else if (number > BT[node]) {
+                node = node * 2 + 1;
+            }
+        }
+        System.out.printf("%d는 현재 BT에 존재하지 않음\n",number);
+    }
+
+    private void arrExtends(int length) {
+        this.BT = Arrays.copyOf(this.BT, length);
+    }
+
+    public Integer[] getBT() {
+        return BT;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import BinaryTree.BinaryTree;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BinaryTree BT = new BinaryTree();
+        BT.insert(5);
+        BT.insert(3);
+        BT.insert(7);
+        BT.insert(89);
+        BT.insert(1);
+        BT.insert(3);
+        BT.insert(8);
+        BT.findValueIndex(89);  //89은 7번째 인덱스에 위치함
+        System.out.println(Arrays.toString(BT.getBT())); //[null, 5, 3, 7, 1, null, null, 89, null, null, null, null, null, null, null, null, null, null, null, null]
     }
 }
