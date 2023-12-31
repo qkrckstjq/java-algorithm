29480718578354b3e35ebf7a4bb4982f5af246c1
qkrckstjq <cksk49@naver.com>
2023-12-31 18:20:15 +0900

    feat MergeSort 제네릭 변환

--- src/MergeSort/MergeSort.java
+++ src/MergeSort/MergeSort.java
@@ -1,15 +1,16 @@
 package MergeSort;
 
-public class MergeSort {
-    public static int[] sort (int[] arr) {
-        return merge(arr, 0, arr.length-1);
+public class MergeSort<T extends Comparable<T>> {
+    public static <T extends Comparable<T>> void sort (T[] arr) {
+        T[] sortedArray = merge(arr, 0, arr.length - 1);
+        System.arraycopy(sortedArray, 0, arr, 0, arr.length);
     }
-    private static int[] sorting (int[] left, int[] right) {
-        int[] result = new int[left.length + right.length];
+    private static <T extends Comparable<T>> T[] sorting (T[] left, T[] right) {
+        T[] result = (T[]) new Comparable[left.length + right.length];
         int i = 0, j = 0;
         int idx = 0;
         while(i < left.length && j < right.length) {
-            if(left[i] > right[j]) {
+            if(left[i].compareTo(right[j]) > 0) {
                 result[idx] = right[j];
                 j++;
             } else {
@@ -31,15 +32,15 @@ public class MergeSort {
 
         return result;
     }
-    private static int[] merge (int[] arr, int startIndex, int endIndex) {
+    private static <T extends Comparable<T>> T[] merge (T[] arr, int startIndex, int endIndex) {
         if(startIndex >= endIndex) {
-            int[] temp = new int[1];
+            T[] temp = (T[]) new Comparable[1];
             temp[0] = arr[startIndex];
             return temp;
         }
         int midIndex =  (startIndex+endIndex) / 2;
-        int[] left = merge(arr, startIndex, midIndex);
-        int[] right = merge(arr, midIndex+1, endIndex);
+        T[] left = merge(arr, startIndex, midIndex);
+        T[] right = merge(arr, midIndex+1, endIndex);
         return sorting(left, right);
     }
 }
