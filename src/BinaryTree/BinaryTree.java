4d466a2347e59a72b3725f3674a9604f174ee278
qkrckstjq <cksk49@naver.com>
2023-12-31 18:40:23 +0900

    feat 이진트리 제네릭화

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -1,25 +1,24 @@
 package BinaryTree;
 
 import Utils.ArrayUtils;
-import java.util.Arrays;
 
-public class BinaryTree {
+public class BinaryTree<T extends Comparable<T>> {
     private int defaultSize = 10;
-    private Integer[] BT;
+    private T[] BT;
     public BinaryTree() {
-        this.BT = new Integer[defaultSize];
+        this.BT = (T[]) new Comparable[defaultSize];
         this.BT[0] = null;
     }
-    public void insert(int number) {
+    public void insert(T number) {
         int currentIndex = 1;
 
         while (currentIndex < BT.length) {
             if (BT[currentIndex] == null) {
                 BT[currentIndex] = number;
                 break;
-            } else if (number < BT[currentIndex]) {
+            } else if (number.compareTo(BT[currentIndex]) < 0) {
                 currentIndex = currentIndex * 2; // 왼쪽 자식으로 이동
-            } else if (number > BT[currentIndex]) {
+            } else if (number.compareTo(BT[currentIndex]) > 0) {
                 currentIndex = currentIndex * 2 + 1; // 오른쪽 자식으로 이동
             } else {
                 break;
@@ -32,22 +31,22 @@ public class BinaryTree {
         }
     }
 
-    public void findValueIndex (int number) {
+    public void findValueIndex (T number) {
         int node = 1;
         while(BT[node] != null) {
             if(BT[node] == number) {
                 System.out.printf("%d은 %d번째 인덱스에 위치함\n", number, node);
                 return;
             }
-            if(number < BT[node]) {
+            if(number.compareTo(BT[node]) < 0) {
                 node = node * 2;
-            } else if (number > BT[node]) {
+            } else if (number.compareTo(BT[node]) > 0) {
                 node = node * 2 + 1;
             }
         }
         System.out.printf("%d는 현재 BT에 존재하지 않음\n",number);
     }
-    public Integer[] getBT() {
+    public T[] getBT() {
         return BT;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,51 @@ import MergeSort.MergeSort;
 import PriorityQueue.PriorityQueue;
 import QuickSort.QuickSort;
 import java.util.Arrays;
+import BinaryTree.BinaryTree;
 
 public class Main {
     public static void main(String[] args) {
-//        BinaryTree BT = new BinaryTree();
-//        BT.insert(5);
-//        BT.insert(3);
-//        BT.insert(7);
-//        BT.insert(89);
-//        BT.insert(1);
-//        BT.insert(3);
-//        BT.insert(14);
-//        BT.insert(15);
-//        BT.insert(734);
-//        BT.insert(12);
-//        BT.insert(3457);
-//        BT.insert(657);
-//        BT.insert(7344);
-//        BT.insert(123);
-//        BT.insert(7547);
-//        BT.insert(4868);
-//        BT.findValueIndex(123);  //123은 60번째 인덱스에 위치함
-//        System.out.println(Arrays.toString(BT.getBT()));
+        BinaryTree BT = new BinaryTree();
+        BT.insert(5);
+        BT.insert(3);
+        BT.insert(7);
+        BT.insert(89);
+        BT.insert(1);
+        BT.insert(3);
+        BT.insert(14);
+        BT.insert(15);
+        BT.insert(734);
+        BT.insert(12);
+        BT.insert(3457);
+        BT.insert(657);
+        BT.insert(7344);
+        BT.insert(123);
+        BT.insert(7547);
+        BT.insert(4868);
+        BT.findValueIndex(123);  //123은 60번째 인덱스에 위치함
+        System.out.println(Arrays.toString(BT.getBT()));
 // ------------------------------------------------------------
 
-        PriorityQueue PQ = new PriorityQueue();
-        PQ.insert(1);
-        PQ.insert(5);
-        PQ.insert(6);
-        PQ.insert(16);
-        PQ.insert(81);
-        PQ.insert(91);
-        PQ.insert(451);
-        PQ.insert(631);
-        PQ.insert(12);
-        System.out.println(Arrays.toString(PQ.getPQ()));
-        PQ.removePeek();
-        System.out.println(Arrays.toString(PQ.getPQ()));
-        PQ.removePeek();
-        System.out.println(Arrays.toString(PQ.getPQ()));
-        PQ.removePeek();
-        System.out.println(Arrays.toString(PQ.getPQ()));
-        PQ.removePeek();
-        System.out.println(Arrays.toString(PQ.getPQ()));
-        PQ.removePeek();
+//        PriorityQueue PQ = new PriorityQueue();
+//        PQ.insert(1);
+//        PQ.insert(5);
+//        PQ.insert(6);
+//        PQ.insert(16);
+//        PQ.insert(81);
+//        PQ.insert(91);
+//        PQ.insert(451);
+//        PQ.insert(631);
+//        PQ.insert(12);
+//        System.out.println(Arrays.toString(PQ.getPQ()));
+//        PQ.removePeek();
+//        System.out.println(Arrays.toString(PQ.getPQ()));
+//        PQ.removePeek();
+//        System.out.println(Arrays.toString(PQ.getPQ()));
+//        PQ.removePeek();
+//        System.out.println(Arrays.toString(PQ.getPQ()));
+//        PQ.removePeek();
+//        System.out.println(Arrays.toString(PQ.getPQ()));
+//        PQ.removePeek();
 // ----------------------------------------------------
 //        int[] arr = {1,3,2,51,12,3,6,2,4,7,78};
 
